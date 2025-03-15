d7498a23454ed532bfdf1a6fc4326b96eacdab9d
Mahthy Hasan <mahthyhasan01@gmail.com>
2025-03-16 02:56:37 +0530

    2-Configure Redux Store and Firebase Auth

---
+++ myApp/src/action/authActions.js
@@ -0,0 +1,46 @@
+import authService from "../service/authService";
+import { setUser, setLoading, setError, logout } from "./authSlice";
+
+export const registerUser = (email, password) => async (dispatch) => {
+  dispatch(setLoading(true));
+  try {
+    const user = await authService.registerUser(email, password);
+    dispatch(setUser(user));
+  } catch (error) {
+    dispatch(setError(error.message));
+  }
+  dispatch(setLoading(false));
+};
+
+export const loginUser = (email, password) => async (dispatch) => {
+  dispatch(setLoading(true));
+  try {
+    const user = await authService.loginUser(email, password);
+    dispatch(setUser(user));
+  } catch (error) {
+    dispatch(setError(error.message));
+  }
+  dispatch(setLoading(false));
+};
+
+export const logoutUser = () => async (dispatch) => {
+  try {
+    await authService.logoutUser();
+    dispatch(logout());
+  } catch (error) {
+    dispatch(setError(error.message));
+  }
+};
+
+export const loadUser = () => async (dispatch) => {
+  dispatch(setLoading(true));
+  try {
+    const user = await authService.getStoredUser();
+    if (user) {
+      dispatch(setUser(user));
+    }
+  } catch (error) {
+    dispatch(setError(error.message));
+  }
+  dispatch(setLoading(false));
+};
